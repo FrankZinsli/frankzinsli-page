47df6f79b4d63b1402a18708f875d99863a94414
Frank Zinsli <frank.zinsli@gmail.com>
2023-08-28 00:26:29 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,11 +1,13 @@
 <template>
   <NuxtLayout><NuxtPage /></NuxtLayout>
 </template>
-<script lang="ts" setup>useSeoMeta({
-  title: 'My Amazing Site',
-  ogTitle: 'My Amazing Site',
-  description: 'This is my amazing site, let me tell you all about it.',
-  ogDescription: 'This is my amazing site, let me tell you all about it.',
-  ogImage: 'https://example.com/image.png',
-  twitterCard: 'summary_large_image',
-})</script>
+<script lang="ts" setup>
+  useSeoMeta({
+    title: 'My Amazing Site',
+    ogTitle: 'My Amazing Site',
+    description: 'This is my amazing site, let me tell you all about it.',
+    ogDescription: 'This is my amazing site, let me tell you all about it.',
+    ogImage: 'https://example.com/image.png',
+    twitterCard: 'summary_large_image',
+  })
+</script>
