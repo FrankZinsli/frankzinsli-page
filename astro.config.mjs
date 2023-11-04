4116f69effe717f6bbd074bf86436f03f3588d71
Frankie1996 <frankie_96@windowslive.com>
2023-11-04 12:10:30 +0100

    feat(astro.config.mjs): add site, Compress, sitemap, and robotsTxt configurations to improve SEO and performance chore(package.json): update dependencies and add astro-compress to devDependencies for better compression feat(LayoutProject.astro): add sitemap link to improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,34 @@
 import { defineConfig, squooshImageService } from 'astro/config';
 import prefetch from "@astrojs/prefetch";
-
+import Compress from "astro-compress";
 import vue from "@astrojs/vue";
+import robotsTxt from 'astro-robots-txt';
+import sitemap from "@astrojs/sitemap";
+
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 21123 },
+  site: 'https://frankzinsli.ch',
+  server: {
+    port: 21123
+  },
   router: 'spa',
-  integrations: [prefetch(), vue()],
+  integrations: [prefetch(), vue(), Compress({
+    CSS: true,
+    HTML: true,
+    JavaScript: true,
+    SVG: true,
+    Image: true
+  }), sitemap({
+    changefreq: 'weekly',
+    priority: 1,
+    lastmod: new Date()
+  }), robotsTxt({
+    host: 'frankzinsli.ch',
+    sitemap: [
+      'https://frankzinsli.ch/sitemap-index.xml'
+    ],
+  })],
   image: {
     service: squooshImageService()
   }
